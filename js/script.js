7391e8a84d7f1dbc7d2d6f78216177134243e7b1
James Thorne <james@jamesthorne.ca>
2013-07-12 10:02:07 -0600

    Added professional experience, minor tweaks

--- js/script.js
+++ js/script.js
@@ -1,5 +1,10 @@
 $(document).ready(function() {
 
+    var expandalob_visible = { "1": false,
+                               "2": false };
+    var other_expandalob = { "1": "2", 
+                             "2": "1" };
+
     $(".video_pick").bind("click", function(e) {
 
         var new_iframe = $(e.target).attr("data-youtube");
@@ -16,6 +21,56 @@ $(document).ready(function() {
                 $(obj).removeClass("active");
             }
         });
+    });
+
+    var setExpandalobVisible = function(expandalob, visible) {
+        var isVisibleAlready = expandalob_visible[expandalob];
+
+        if (visible == isVisibleAlready) return;
+
+        if (visible)
+        {
+            $(".expandalob_" + expandalob).slideDown();
+        }
+        else
+        {
+            $(".expandalob_" + expandalob).slideUp();
+        }
+        expandalob_visible[expandalob] = visible;
+    };
+
+    $(".main_body").bind("click", function(e) {
+        setExpandalobVisible("1", false);
+        setExpandalobVisible("2", false);
+    });
+
+    $(".probutton").bind("click", function(e) {
+
+        var t = $(e.target);
+        if (t.attr("data-position") == undefined)
+        {
+            t = $(e.target.parentNode);
+        }
+
+        var position = t.attr("data-position");
+        var expandalob = t.attr("data-expandalob");
+        var contentclass = t.attr("data-content-class");
+
+        $(".expand_content").hide();
+        $(".expand_content_" + contentclass).show();
+
+        if (expandalob_visible[expandalob])
+        {
+            $(".expandalob_arrow_" + expandalob).stop(true, true).animate( {"margin-left": position} );
+        }
+        else
+        {
+            $(".expandalob_arrow_" + expandalob).stop(true, true).css("margin-left", position);
+        }
+        setExpandalobVisible(expandalob, true);
+        setExpandalobVisible(other_expandalob[expandalob], false);
+
+        return false;
     });
 
 });
