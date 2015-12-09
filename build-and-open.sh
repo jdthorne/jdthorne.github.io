6ec1bdf05a545d608c1b0aba357d90e494b314ac
James Thorne <james@jamesthorne.ca>
2015-12-09 15:54:56 -0700

    Added WhiteSky offline page

--- build-and-open.sh
+++ build-and-open.sh
@@ -4,4 +4,8 @@ browserify -t coffeeify src/index.coffee -o obj/index.js || exit
 lessc src/index.less > obj/index.css || exit
 slimrb src/index.slim > index.html || exit
 
+browserify -t coffeeify src/whitesky.coffee -o obj/whitesky.js || exit
+lessc src/whitesky.less > obj/whitesky.css || exit
+slimrb src/whitesky.slim > whitesky.html || exit
+
 open index.html
